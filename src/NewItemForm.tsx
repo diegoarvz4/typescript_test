7127b01aed979683f35d3379b7b45cca6824344f
DiegoArvz <darvz.gtz@gmail.com>
2020-06-22 19:37:14 -0500

    TypeScript project

---
+++ src/NewItemForm.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { useFocus } from './utils/useFocus';
+import { NewItemFormContainer, NewItemButton, NewItemInput } from './styles';
+
+interface NewItemProps {
+  onAdd(text: string): void
+}
+
+export const NewItemForm = ({ onAdd } : NewItemProps ) => {
+  const [text, setText] = useState("");
+  const inputRef = useFocus();
+
+  return (
+    <NewItemFormContainer>
+      <NewItemInput
+        ref={inputRef} 
+        value={text} 
+        onChange={e => setText(e.target.value)}
+      />
+      <NewItemButton onClick={() => onAdd(text)}>
+        Create
+      </NewItemButton>
+    </NewItemFormContainer>
+  )
+}
